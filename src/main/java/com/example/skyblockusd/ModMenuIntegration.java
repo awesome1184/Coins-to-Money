6d9392799a8ba4cf8285dce50a294cd19bcdafe7
awesome1184 <awesome69421@outlook.com>
2026-09-13 18:21:39 -0500

    Integrate precision settings with Mod Menu

---
+++ src/main/java/com/example/skyblockusd/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package com.example.skyblockusd;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.screens.Screen;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return CoinsToMoneyConfigScreen::new;
+    }
+}
